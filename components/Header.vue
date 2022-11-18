829b6fefc7af5c65dbc403e25f7a706cb7613a11
RodrigoFantechi <rodrifante595@libero.it>
2022-11-18 17:08:33 +0100

    add components and pages

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+const runtimeConfig = useRuntimeConfig()
+const appConfig = useAppConfig()
+</script>
+<template>
+    <h1>{{runtimeConfig.title}}</h1>
+    <h2>{{appConfig.title}}</h2>
+</template>
+
+<style lang="scss">
+ 
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,7 @@
+export default defineNuxtConfig({
+
+    runtimeConfig: {
+        title:'ciao' 
+    }
+
+})
